18d17226610fb50b9c1eca5dfd256240e6e58faa
Guilherme Monteiro <guibezerra_monteiro@hotmail.com>
2024-05-14 21:00:34 -0300

    Inicio do projeto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small project to recommend movies based on my Letterboxed profile.',
+    author='Guilherme Monteiro',
+    license='MIT',
+)
