a4395e8c935129d43f7e2b11406579ffcb6466cc
Guilherme Monteiro <guibezerra_monteiro@hotmail.com>
2024-05-19 18:24:43 -0300

    Finalizando web scraper e Pg connection

--- notebooks/PgConnection.py
+++ notebooks/PgConnection.py
@@ -1,21 +1,42 @@
+# Imports
 import pandas as pd
 from sqlalchemy import create_engine, MetaData, Table, text
 from sqlalchemy.exc import SQLAlchemyError
 import os
 import logging
+import psycopg2
+from psycopg2 import sql
+import numpy as np
 
 # Set up logging
 logging.basicConfig(level=logging.ERROR)
 
-# Entering database
+# Database connection parameters
 db_user = os.getenv("DB_USER")
 db_host = os.getenv("DB_HOST")
 db_pswd = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
 
-def ExecuteQuerry(query):
+config = {
+    'dbname': db_name,
+    'user': db_user,
+    'password': db_pswd,
+    'host': db_host
+}
+
+
+print(config)
+
+
+# Define database connection URL
+db_url = f'postgresql+psycopg2://{db_user}:{db_pswd}@{db_host}/{db_name}'
+
+# Create SQLAlchemy engine
+engine = create_engine(db_url)
+
+# Function to execute queries
+def ExecuteQuery(query):
     try:
-        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pswd}@{db_host}/{db_name}')
         with engine.connect() as connection:
             result = connection.execute(text(query))
         return result
@@ -23,9 +44,9 @@ def ExecuteQuerry(query):
         logging.error(f"An error occurred while executing the query: {e}")
         return None
 
+# Function to return DataFrame from query
 def ReturningDF(query):
     try:
-        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pswd}@{db_host}/{db_name}')
         with engine.connect() as connection:
             result = connection.execute(text(query))
             df = pd.DataFrame(result.fetchall(), columns=result.keys())
@@ -34,11 +55,9 @@ def ReturningDF(query):
         logging.error(f"An error occurred: {e}")
         return None
 
+# Function to enter data into a table
 def EnteringTable(table_name, df_enter):
     try:
-        # Database connection
-        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pswd}@{db_host}/{db_name}')
-        
         # Reflect existing table structure
         metadata = MetaData()
         metadata.reflect(bind=engine)
@@ -60,26 +79,23 @@ def EnteringTable(table_name, df_enter):
     except SQLAlchemyError as e:
         logging.error(f"An error occurred: {e}")
         return None
+
+# Function to add a row to a table
+def AddRowToTable(movie_data):
+    # Convert numpy types to native Python types
+    converted_data = tuple(int(item) if isinstance(item, np.integer) else item for item in movie_data)
     
+    # Create a SQL query with the table name included
+    query = """INSERT INTO public.moviesdb ("Date", "Name", "Year", "Letterboxd_URI", "Director", "Genres", "Themes", "Actors") 
+               VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
     
-def AddRowToTable(table_name, df_row):
     try:
-        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pswd}@{db_host}/{db_name}')
-        with engine.connect() as connection:
-            # Convert the pandas row to a dictionary
-            row_dict = df_row.to_dict()
-            
-            # Generate the columns and values for the INSERT statement
-            columns = ', '.join(row_dict.keys())
-            values = ', '.join([f":{key}" for key in row_dict.keys()])
-            
-            # Prepare the SQL statement
-            sql = text(f"INSERT INTO {table_name} ({columns}) VALUES ({values})")
-            
-            # Execute the SQL statement
-            connection.execute(sql, **row_dict)
-        
-        logging.info(f'Row added to {table_name} table successfully.')
-    except SQLAlchemyError as e:
-        logging.error(f"An error occurred: {e}")
-        return None
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # Execute the INSERT statement
+                cur.execute(query, converted_data)
+                # Commit the transaction
+                conn.commit()
+                print("Movie inserted successfully")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
